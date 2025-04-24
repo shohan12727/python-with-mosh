e78941224d2dd927f2613cbf8abc47e96fba5157
shohan12727 <aishohan001@gmail.com>
2025-04-24 18:46:19 +0600

    enough for today

---
+++ formattedString.py
@@ -0,0 +1,15 @@
+# first = 'shohan'
+# last = 'islam'
+# msg = f'{first} {last} is a coder'
+# print(msg)
+
+
+statement = 'I love javaScript'
+print (len(statement))
+print(statement.upper())
+print (statement.lower())
+print(statement.find('o'))
+
+print (statement.replace('javaScript','python'))
+
+print (statement.find('love'))
---
+++ strings.py
@@ -0,0 +1,7 @@
+statement = "amar sonar bangla ami tomay valobasi "
+
+print (statement)
+
+print(statement[1:])
+print(statement[0:-1])
+
