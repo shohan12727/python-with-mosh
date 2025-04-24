c6edecaaa88d69681d1fbc83de192d9c1c4ed136
shohan12727 <aishohan001@gmail.com>
2025-04-24 16:48:43 +0600

    updated

--- typeConversion.py
+++ typeConversion.py
@@ -1,3 +1,11 @@
 birth_year = input("birth year: ")
-age = 2025-birth_year
+age = 2025-int(birth_year)
 print(age)
+
+poundWeight=input("what is your weight in pound? ")
+kgWeight=int(poundWeight)*0.453592
+
+print("your weight in kg is "+str(kgWeight))
+
+
+
