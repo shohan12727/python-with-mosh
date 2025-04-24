b6f745b39349822872d6a4658bbbeae2f446270f
shohan12727 <aishohan001@gmail.com>
2025-04-24 16:08:26 +0600

    update

--- app.py
+++
@@ -1,3 +0,0 @@
-print("Shohan")
-print("o---")
-print("  ||||")
---
+++ receivingInput.py
@@ -0,0 +1,8 @@
+name = input ("what is your name ?")
+print("hi "+name)
+
+personName = input("what is your name ?")
+personColor = input("what is your favourite color?")
+
+print(personName + " likes " + personColor)
+
